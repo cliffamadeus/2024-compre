631624a93e440a7aedcec61615300385dbc4ccee
Cliffy <dev.clfm5t12@gmail.com>
2024-01-12 06:23:17 +0800

    Added solution2 to for problem 2

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -11,17 +11,34 @@ package problem2;
  */
 public class Main {
      public static void main(String[] args) {
-        // TODO code application logic here
+        //Solution 1
+    	System.out.println("Solution 2");
         char[] name = {'N', 'B', 'S', 'C'};
         
         for (int i = 0; i < name.length; i++) {
             System.out.print(name[i]);
         }
-        System.out.println("");
+        System.out.println();
         
         for (int i = name.length-1; i >= 0; i--) {  
             System.out.print(name[i]);  
         }
-        System.out.println("");
+        System.out.println();
+        System.out.println();
+        
+        //Solution 2
+        System.out.println("Solution 2");
+        String school = "NBSC";
+        char[] name2 = school.toCharArray();
+        
+        for (int i = 0; i < name2.length ; i++) {
+        	System.out.print(name2[i]);
+        }
+        System.out.println();
+        
+        for (int i = name2.length-1; i>=0; i--) {
+        	System.out.print(name2[i]);
+        }
+        
     }
 }
