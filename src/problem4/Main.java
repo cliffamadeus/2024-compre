3d49a3a80902b63dbe131cc66a0947d53dac8a7f
Cliffy <dev.clfm5t12@gmail.com>
2024-01-11 18:53:13 +0800

    Added Problem 4 Solution

--- src/problem4/Main.java
+++ src/problem4/Main.java
@@ -11,23 +11,22 @@ package problem4;
  */
 public class Main {
       public static void main(String[] args) {
-        // TODO code application logic here
-        /*
-        int[] num = {1, 2, 3, 4, 5};
-        */
-        
-        int[][] num = { {1, 2, 3, 4}, {5, 6, 7} };
-        
-        int sum=0;
-        int avg;
+      
+        int[][] num = { {1, 2, 3, 4}, {5, 6, 7},{3,1,2,4 }};
+             
         for (int i = 0; i < num.length; ++i) {
-            for(int j = 0; j < num[i].length; ++j) {
-              System.out.print(num[i][j]);
-
+            int sum = 0;
+            for (int j = 0; j < num[i].length; ++j) {
+                System.out.print(num[i][j] + " ");
+                sum += num[i][j];
             }
+            
+            int avg = sum / num[i].length;
+            
+            System.out.println();
+            System.out.println("Sum for array " + i + ": " + sum);
+            System.out.println("Average for array " + i + ": " + avg);
+            System.out.println();
         }
-        
-        avg = sum/num.length;
-        System.out.println(avg);
     }  
 }
