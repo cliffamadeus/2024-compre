4bb90cc7ba48666be3ae8ac4958b4811aa976aa7
Cliffmeister <dev.clfm5t12@gmail.com>
2024-01-15 21:10:01 +0800

    Minor solution changes for problem 4

--- src/problem4/Main.java
+++ src/problem4/Main.java
@@ -13,7 +13,7 @@ public class Main {
         for (int i=0; i<num.length;i++) {
         	double sum = 0;
         	for(int j=0;j<num[i].length;j++) {
-        		System.out.print(num[i][j]);
+        		System.out.print(" "+num[i][j]);
         		sum +=num[i][j];
         	}
         	
